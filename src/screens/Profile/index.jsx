214a0ace9e9abb254c49d544cd44a2dc8388d642
JuliaCastro-dev <juliaprofessional21@gmail.com>
2023-09-12 23:28:40 -0300

    atualização do perfil

--- src/screens/Profile/index.jsx
+++ src/screens/Profile/index.jsx
@@ -1,49 +1,45 @@
 import Header from "../../components/Header";
 import {
-  CardBio,
-  ProfileBio,
+  Card,
+  SubTitle,
   ProfileContainer,
   ProfileImage,
   ProfileName,
-  Wallpaper,
+  Bio,
+  Data,
+  Value,
+  Wallpaper
 } from "./styles";
 import foto from "../../images/homem2.jpg";
 
 const Profile = () => {
   const profile = {
     name: "John Doe",
-    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor lorem sit amet velit dictum efficitur. Cras euismod vestibulum lobortis. Vestibulum posuere dolor lacus, a finibus purus tempor eu. Curabitur sollicitudin mi at magna euismod, vel laoreet sapien cursus. Nulla bibendum orci at quam venenatis lacinia. Vivamus nec ipsum at nisl convallis ullamcorper. Proin quis venenatis lorem.",
+    city:"São Paulo, SP",
+    bio: "Sou Lucas Martins, um engenheiro ambiental de São Paulo com 35 anos de idade. Desde muito jovem, minha paixão pelos animais e pelo meio ambiente moldou meu caminho na vida. Hoje, sou um defensor da natureza e um dedicado protetor dos animais.",
     image: foto, // Replace with the actual image URL
   };
   return (
     <ProfileContainer>
-      <Header />
-      <Wallpaper
-        src={
-          "https://img.freepik.com/fotos-gratis/fundo-gradiente-de-grade-digital-futurista-azul-escuro_53876-129728.jpg?w=1380&t=st=1686351210~exp=1686351810~hmac=72d5eb1348f5f1a1680041b444f74b93d0a23a77f9a0907c5508b24d8f10105a"
-        }
-        alt="Imagem de rio com mata com o nome schneider"
-      />
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-          position: "relative",
-          left: "8vw",
-          width: "100%",
-          marginTop: "27vh",
-        }}
-      >
+      <Header/>
+      <Card>
+        <Wallpaper/>
         <ProfileImage src={profile.image} alt={profile.name} />
-        <ProfileName>{profile.name}</ProfileName>
-      </div>
-
-      <CardBio>
-        <strong> E-mail:</strong> John.doe@schneider.com.br
-        <strong> Departamento:</strong> Marketing Digital
-        <strong> Celular:</strong> (11) 99944-9944
-      </CardBio>
+        <Value style={{ fontSize:'24px'}}>{profile.name}</Value>
+        <Value style={{fontWeight:400}}>São Paulo, São Paulo, Brasil</Value>
+        <Data>
+          <SubTitle> E-mail</SubTitle>
+          <Value>John.doe@schneider.com.br</Value>
+          <SubTitle>Departamento</SubTitle>
+          <Value>Marketing Digital</Value>
+          <SubTitle>Polo</SubTitle>
+          <Value>Schneider São Paulo</Value>
+          <SubTitle>Celular</SubTitle>
+          <Value>(11) 99944-9944</Value>
+        </Data>
+        <Value>Sobre mim</Value>
+        <Bio>{profile.bio}</Bio>
+      </Card>
     </ProfileContainer>
   );
 };
--- src/screens/Profile/styles.js
+++ src/screens/Profile/styles.js
@@ -3,13 +3,17 @@ import styled from "styled-components";
 export const ProfileContainer = styled.div`
   display: flex;
   flex-direction: column;
-  backgroundcolor: #d3d3d3;
+  background-color: #f1f5f9;
+  height: 100vh;
+  width: 100vw;
+  align-items: center;
 `;
 
 export const ProfileImage = styled.img`
   width: 200px;
   height: 190px;
   border-radius: 50%;
+  z-index: 2;
 `;
 
 export const ProfileName = styled.h2`
@@ -25,25 +29,54 @@ export const ProfileBio = styled.p`
   line-height: 1.5;
   text-align: center;
 `;
-
-export const Wallpaper = styled.img`
-  width: 100vw;
-  height: 49vh;
-  position: absolute;
-  top: 10vh;
-  left: 0;
-  z-index: -1;
-  display: flex;
+export const Wallpaper = styled.div`
+  background-image: url(https://4.bp.blogspot.com/-7_7Xk44sL28/Un0I7-PQ40I/AAAAAAAAiuM/VcAudz39qcE/s1600/Building-arco-pedra-arquitetura-hdr-485x728.jpg); /* Set the wallpaper image as background */
+  background-size: cover; /* Ensure the image covers the entire container */
+  background-position: center; /* Center the image */
 `;
-export const CardBio = styled.div`
-  width: 900px;
+export const Card = styled.div`
+  width: 80vw;
+  height: 80vh;
   background-color: #ffffff;
-  border-radius: 8px;
-  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+
+  border-radius: 20px;
   padding: 20px;
-  margin-bottom: 20px;
-  position: relative;
-  margin-left: 20vw;
+  margin-top: 5vh;
   display: flex;
   flex-direction: column;
+
+  align-items: center;
+  justify-content: center;
+  box-shadow: 4px 5px 15px 0px #606090;
+`;
+export const Data = styled.div`
+  width: 90%;
+  background-color: #ffffff;
+  border-radius: 10px;
+  padding: 20px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  max-height: 30%;
+`;
+export const SubTitle = styled.p`
+  background-color: #009530;
+  font-size: 0.7vw;
+  color: #ffffff;
+  font-weight: 600;
+  padding: 5px;
+  width: 120px;
+  position: relative;
+  text-align: center;
+  border-radius: 7px;
+`;
+export const Value = styled.p`
+  font-size: 1rem;
+  color: #717171;
+  position: relative;
+  font-weight: 600;
+`;
+export const Bio = styled.cite`
+  width: 60%;
 `;
